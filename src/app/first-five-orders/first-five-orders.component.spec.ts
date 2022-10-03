db605b2c48fba4e0953d45af7871f3f036ef1e06
Ben McMullin <46494991+benmcmullin@users.noreply.github.com>
2022-10-03 14:23:17 -0500

    ADD: Components

---
+++ src/app/first-five-orders/first-five-orders.component.spec.ts
@@ -0,0 +1,23 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { FirstFiveOrdersComponent } from './first-five-orders.component';
+
+describe('FirstFiveOrdersComponent', () => {
+  let component: FirstFiveOrdersComponent;
+  let fixture: ComponentFixture<FirstFiveOrdersComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ FirstFiveOrdersComponent ]
+    })
+    .compileComponents();
+
+    fixture = TestBed.createComponent(FirstFiveOrdersComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
